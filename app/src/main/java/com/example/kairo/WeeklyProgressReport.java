faf57103643d5ee576dffafa6b467be168d0b3fc
robinranaprofessional-commits <robinranaprofessional@gmail.com>
2025-11-17 20:59:06 -0800

    revived some files that were deleeted

--- app/src/main/java/com/example/kairo/WeeklyProgressReport.java
+++ app/src/main/java/com/example/kairo/WeeklyProgressReport.java
@@ -10,6 +10,15 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
 
 public class WeeklyProgressReport extends AppCompatActivity {
     //Initialize variable
@@ -22,8 +31,57 @@ public class WeeklyProgressReport extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_weekly_progress_report);
         //Assign variable
-        barChart = findViewById(R.id.bar_Chart);
-        pieChart = findViewById(R.id.pieChart);
+        barChart = findViewById(R.id.bar_chart);
+        pieChart = findViewById(R.id.pie_chart);
+
+        //Initializing Array list, this is a dummy dataset
+        ArrayList<BarEntry> barEntries = new ArrayList<>();
+        ArrayList<PieEntry> pieEntries = new ArrayList<>();
+
+        //Use for loop
+        for(int i =1; i<10; i++)
+        {
+            float value  = (float) (i*10);
+            //initalize bar entry
+            BarEntry barEntry = new BarEntry(i, value);
+            //initialize pie entry
+            PieEntry pieEntry = new PieEntry(value);
+
+            //Add to array list
+            barEntries.add(barEntry);
+            pieEntries.add(pieEntry);
+        }
+
+        //Intialize bar data set
+        BarDataSet barDataSet = new BarDataSet(barEntries, "Day of Week");
+        //Set Colors
+        barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        //Hiding draw values
+        barDataSet.setDrawValues(false);
+        //set bar data set
+        barChart.setData(new BarData(barDataSet));
+        //ANIMATion
+        barChart.animateY(5000);
+        //set Description text and color
+        barChart.getDescription().setText("Day of Week");
+        barChart.getDescription().setTextColor(R.color.darkCream);
+
+
+        //Initialize pie data set
+        PieDataSet pieDataSet = new PieDataSet(pieEntries, "Pie Chart");
+        //set colors
+        pieDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        //set pie data set
+        pieChart.setData(new PieData(pieDataSet));
+        //animation
+        pieChart.animateXY(5000, 5000);
+        //hide description
+        pieChart.getDescription().setEnabled(false);
+
+
+
+
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
