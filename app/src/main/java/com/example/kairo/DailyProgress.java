0700a45d56384949e91a9598d55a82463df26bf9
PSG109 <gillparteek101@gmail.com>
2025-12-04 19:48:00 -0800

    removed a comment

--- app/src/main/java/com/example/kairo/DailyProgress.java
+++ app/src/main/java/com/example/kairo/DailyProgress.java
@@ -64,32 +64,3 @@ public class DailyProgress extends Activity {
 
 
 
-/* package com.example.kairo;
-
-
-import android.os.Bundle;
-import android.widget.Button;
-import android.widget.TextView;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-
-public class DailyProgress extends AppCompatActivity {
-
-    Button btnStartSession, btnViewStats;
-    TextView tvAddHabit;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_daily_progress);
-
-        btnStartSession = findViewById(R.id.btnStartSession);
-        btnViewStats = findViewById(R.id.btnViewStats);
-        tvAddHabit = findViewById(R.id.tvAddHabit);
-
-    }
-}
-
-
-*/
