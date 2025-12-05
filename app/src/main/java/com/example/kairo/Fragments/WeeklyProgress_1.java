e7f34f8fadfafeb2cb7d4505a9a66c6d9a1bc5a0
KD8610 <dobik22@mytru.ca>
2025-12-05 13:54:40 -0800

    Logout button functionality

--- app/src/main/java/com/example/kairo/Fragments/WeeklyProgress_1.java
+++ app/src/main/java/com/example/kairo/Fragments/WeeklyProgress_1.java
@@ -1,24 +1,42 @@
 package com.example.kairo.Fragments;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
+import com.example.kairo.IntroScreen;
 import com.example.kairo.R;
 
 public class WeeklyProgress_1 extends Fragment {
 
-    public WeeklyProgress_1() {
-        // Required empty public constructor
-    }
+    Button btnLogout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_weekly_progress, container, false);
+        View view = inflater.inflate(R.layout.fragment_weekly_progress, container, false);
+
+        btnLogout = view.findViewById(R.id.btnLogout);
+
+        btnLogout.setOnClickListener(v -> {
+            SharedPreferences logoutPrefs = requireActivity().getSharedPreferences("kairo_prefs", Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = logoutPrefs.edit();
+            editor.clear();
+            editor.apply();
+
+            Intent intent = new Intent(requireActivity(), IntroScreen.class);
+            startActivity(intent);
+            requireActivity().finish();
+        });
+
+        return view;
     }
 }
